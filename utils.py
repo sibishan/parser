c76dcfba660e4a1150089c0d82fbf83d7a85e015
Sibishan Ravindran <sibishanr@gmail.com>
2026-08-25 17:59:40 +1000

    Revert "cleaned up lexer utils"
    This reverts commit cd8cda57493e0e8c6db27bb7436c6e6916140fbb.

--- utils.py
+++ utils.py
@@ -1,1 +1,32 @@
+def is_glyph(s):
+    if s in [
+        '+', '×', '∸', '⊤', '⊥', '←', '↑', '→', '↓', '∷', 'Θ'
+    ]:
+        return True
+
+    return False
+
+def is_symbol(s):
+    if s in [
+        '(', ')', 'λ', '.'
+    ]:
+        return True
+
+    return False
+
+def classify(s):
+    if s.isdigit():
+        return "DIGIT"
+    elif 'a' <= s <= 'z' or s == "-":
+        return "LETTER"
+    elif s.isspace():
+        return "SPACE"
+    elif is_glyph(s):
+        return "GLYPH"
+    elif is_symbol(s):
+        return "SYMBOL"
+    elif s == ";":
+        return None
+    else:
+        raise TypeError(f"LEXER: {s} can NOT be classified")
     
